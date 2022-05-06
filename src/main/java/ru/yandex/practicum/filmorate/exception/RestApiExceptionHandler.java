b7b76f4f0abbafee905dc44b4e347abc65e0c386
Dima <tokidoki-rostov@yandex.ru>
2022-05-06 21:25:09 +0300

    Каркас Spring Boot приложения Filmorate с тестами

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/RestApiExceptionHandler.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmorate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class RestApiExceptionHandler {
+    @ExceptionHandler(value = {ValidationException.class})
+    public ResponseEntity<Object> handleException(Exception e) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        return new ResponseEntity<>(e.getMessage(), status);
+    }
+}
